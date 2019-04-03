f15a41a22c02b5fb128e3c718ef38b44a20212bf
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-02 18:09:48 -0600

    WIP Implement GraphQL
    Co-authored-by: J Aaron Brooks-Roberts <jaaronbr.tech@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { User, Pairing, Date } = require('./schema');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
 
 const connect = () => {
   return mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
@@ -12,18 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('The server is running');
-});
-
-app.post('/testendpoint', (req, res) => {
-  const { name, email } = req.body;
-  connect()
-    .then(async connection => {
-      const user = await User.create({ name, email });
-      res.status(201).json(user);
-    })
-    .catch(error => console.log(error));
-});
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 module.exports = app;
