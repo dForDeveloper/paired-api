2c2190402cdb9e7e0f89fed41dbc1acb488fd590
Jeo D <e8gea6w5z8id@opayq.com>
2019-04-02 20:27:28 -0600

    Update schema and seed file

--- seed/seeds.js
+++ seed/seeds.js
@@ -1,118 +1,105 @@
-const { User, Pairing, Date } = require('../schema');
+const { User, Pairing } = require('../db/schema');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 
-const users = [
-  new User({
-    name: 'Jeo',
-    pronouns: 'he/him',
-    module: 4,
-    program: 'FE',
-    skills: ['javascript', 'redux'],
-    interests: ['everything'],
-    slack: '@thisguy',
-    email: 'anything@xyz.com',
-    image: 'http://www.cheatsheet.com/wp-content/uploads/2016/01/Close-up-Of-A-Smiling-Man.jpg',
-    pairings: []
-  }),
-  new User({
-    name: 'Aaron',
-    pronouns: 'he/him',
-    module: 4,
-    program: 'BE',
-    skills: ['ruby', 'rails'],
-    interests: ['coding'],
-    slack: '@aaron',
-    email: 'aaron@gmail.com',
-    image: 'http://www.image.com/aaron.jpeg',
-    pairings: []
-  }),
-  new User({
-    name: 'Tiffany',
-    pronouns: 'she/her',
-    module: 4,
-    program: 'FE',
-    skills: ['javascript', 'redux'],
-    interests: ['coding'],
-    slack: '@tiffany',
-    email: 'tiff@gmail.com',
-    image: 'http://www.image.com/tiffany.jpeg',
-    pairings: []
-  }),
-  new User({
-    name: 'Hillary',
-    pronouns: 'she/her',
-    module: 4,
-    program: 'FE',
-    skills: ['react', 'redux'],
-    interests: ['super market sweep'],
-    slack: '@hillstew',
-    email: 'hill@gmail.com',
-    image: 'http://www.image.com/hill.jpeg',
-    pairings: []
-  })
-];
+(async () => {
+  await mongoose.connect(
+    'mongodb://localhost:27017/paired',
+    { useNewUrlParser: true }
+  );
 
-const dates = [
-  new Date({
-    date: 'Tues 9th',
-    timeslots: {
-      morning: [],
-      lunch: [],
-      afternoon: []
+  const users = await User.create(
+    [
+      {
+        name: 'Jeo',
+        pronouns: 'he/him',
+        module: 4,
+        program: 'FE',
+        skills: ['react', 'node'],
+        interests: ['magic the gathering'],
+        slack: '@jeo',
+        email: 'jeo@gmail.com',
+        image: 'http://www.image.com/jeo.jpeg',
+        pairings: []
+      },
+      {
+        name: 'Aaron',
+        pronouns: 'he/him',
+        module: 4,
+        program: 'BE',
+        skills: ['ruby', 'rails'],
+        interests: ['art'],
+        slack: '@aaron',
+        email: 'aaron@gmail.com',
+        image: 'http://www.image.com/aaron.jpeg',
+        pairings: []
+      },
+      {
+        name: 'Tiffany',
+        pronouns: 'she/her',
+        module: 4,
+        program: 'FE',
+        skills: ['react', 'testing'],
+        interests: ['baking'],
+        slack: '@tiffany',
+        email: 'tiff@gmail.com',
+        image: 'http://www.image.com/tiffany.jpeg',
+        pairings: []
+      },
+      {
+        name: 'Hillary',
+        pronouns: 'she/her',
+        module: 4,
+        program: 'FE',
+        skills: ['react', 'redux'],
+        interests: ['super market sweep'],
+        slack: '@hillstew',
+        email: 'hill@gmail.com',
+        image: 'http://www.image.com/hill.jpeg',
+        pairings: []
       }
-  }),
-  new Date({
-    date: 'Thurs 11th',
-    timeslots: {
-      morning: [],
-      lunch: [],
-      afternoon: []
-      }
-  })
-];
+    ]
+  );
 
-const userIds = [];
+  await Pairing.create(
+    [
+      {
+        pairerID: users[0]._id,
+        paireeID: null,
+        date: 'Wed Apr 03 2019',
+        time: 'lunch'
+      },
+      {
+        pairerID: users[0]._id,
+        paireeID: null,
+        date: 'Thu Apr 04 2019',
+        time: 'lunch'
+      },
+      {
+        pairerID: users[2]._id,
+        paireeID: users[0]._id,
+        date: 'Wed Apr 03 2019',
+        time: 'afternoon'
+      },
+      {
+        pairerID: users[1]._id,
+        paireeID: users[0]._id,
+        date: 'Thu Apr 04 2019',
+        time: 'morning'
+      },
+      {
+        pairerID: users[3]._id,
+        paireeID: null,
+        date: 'Wed Apr 03 2019',
+        time: 'morning'
+      },
+      {
+        pairerID: users[3]._id,
+        paireeID: users[2]._id,
+        date: 'Fri Apr 05 2019',
+        time: 'morning'
+      },
+    ]
+  );
 
-Promise.all(users.map(async user => {
-  const newUser = await user.save()
-  userIds.push(newUser._id)
-  return newUser;
-}))
-.then(() => {
-  console.log(userIds);
-  mongoose.disconnect()
-})
-.catch((error) => console.log(error));
-
-const dateIds = [];
-
-Promise.all(dates.map(async date => {
-  const newDate = await date.save()
-  dateIds.push(newDate._id)
-  return newDate;
-}))
-.then(() => mongoose.disconnect())
-.catch((error) => console.log(error));
-
-const pairings = [
-  new Pairing({
-    pairedUserID: userIds[0],
-    dateID: dateIds[0],
-    timeslot: 'morning',
-    role: 'pairer'
-
-  }),
-  new Pairing({
-    pairedUserID: userIds[1],
-    dateID: dateIds[1],
-    timeslot: 'afternoon',
-    role: 'pairee'
-  })
-];
-
-// Promise.all(pairings.map(pairing => {
-//   return paring.save()
-// }))
-// .then(() => mongoose.disconnect())
-// .catch((error) => console.log(error));
+  mongoose.disconnect();
+})();
